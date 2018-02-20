4a97bac4bcf1091719264c198cfee876bf86fcc5
nikitauday <nikita.uday@gmail.com>
2018-02-20 10:58:08 +0000

    Delete Shell1.sh

--- Shell1.sh
+++
@@ -1,9 +0,0 @@
-echo "login name:$LOGNAME"
-echo "User name :$USER"
-echo "Current Shell :$SHELL"
-echo "Home directory :$HOME"
-echo "Operating System :$OSTYPE"
-echo "Current Path setting :$PATH"
-echo "Current working directory:`pwd`" 
-echo "No.of users currently logged in:`who | wc -l`"
-
