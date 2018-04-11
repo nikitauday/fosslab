053dd9e98a59191b2248f0641cb119e142022183
nikitauday <nikita.uday@gmail.com>
2018-04-11 21:57:45 +0530

    Calculator

--- Q6/calc.sh
+++ Q6/calc.sh
@@ -7,26 +7,30 @@ echo "Enter second no."
 read n2
 while [ $i = "y" ]
 do
-	echo "1. Addition"
-	echo "2. Subtraction"
-	echo "3. Multiplication"
-	echo "4. Division"
-	read ch
-	case $ch in
-		1)	sum=`expr $n1 + $n2`
-			echo "Sum :" $sum;;
-		2)	diff=`expr $n1 - $n2`
-			echo "Difference :" $diff;;
-		3)	prod=`expr $n1 \* $n2`
-			echo "Product : " $prod;;
-		4)	div=`expr $n1 / $n2`
-			echo "Quotient : " $div;;
-		*)	echo "Unknown option";;
-	esac
-	echo "Go again? (y/n)"
-	read i
-	if [ $i != "y" ]
-	then
-		exit
-	fi
+        echo "1. Addition"
+        echo "2. Subtraction"
+        echo "3. Multiplication"
+        echo "4. Division"
+        echo "5. Modulus"
+        read ch
+        case $ch in
+                1)      sum=`expr $n1 + $n2`
+                        echo "Sum :" $sum;;
+                2)      diff=`expr $n1 - $n2`
+                        echo "Difference :" $diff;;
+                3)      prod=`expr $n1 \* $n2`
+                        echo "Product : " $prod;;
+                4)      div=`expr $n1 / $n2`
+                        echo "Quotient : " $div;;
+                5)      mod=`expr $n1 % $n2`
+                        echo "Modulus: " $mod;;
+                *)      echo "Unknown option";;
+        esac
+        echo "Go again? (y/n)"
+        read i
+        if [ $i != "y" ]
+        then
+                exit
+        fi
 done
+
